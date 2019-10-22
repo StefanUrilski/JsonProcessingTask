fb90ec4e2e7eb8c951dd7da3bb4a5c8d814050d7
StefanUrilski <urilskistefan@gmail.com>
2019-10-22 10:22:03 +0300

    Console Reader interface and class implementation.

---
+++ src/main/java/util/ConsoleReader.java
@@ -0,0 +1,7 @@
+package util;
+
+public interface ConsoleReader {
+
+    String readLine(String filePath);
+
+}
---
+++ src/main/java/util/ConsoleReaderImpl.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.io.Console;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsoleReaderImpl implements ConsoleReader {
+
+
+    @Override
+    public String readLine(String filePath) {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
