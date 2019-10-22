2d807078ff32ffdca06014680512b6f45bac4829
StefanUrilski <urilskistefan@gmail.com>
2019-10-22 10:18:12 +0300

    Initial commit. Creating engine class.

---
+++ src/main/java/Engine.java
@@ -0,0 +1,8 @@
+public class Engine implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,70 +1,8 @@
-import com.google.gson.Gson;
-import dto.StationImportDto;
-import dto.StationExportDto;
-import util.*;
-
-import java.io.IOException;
-import java.util.*;
-
 public class Main {
 
-    private static final String IMPORT_DATA_PATH =
-            "C:\\Users\\Stefan\\IdeaProjects\\Java Spring MVC\\Json Processing\\src\\main\\resources\\inputDataJsonFile.json";
-
-    private static final String EXPORT_DATA_PATH =
-            "C:\\Users\\Stefan\\IdeaProjects\\Java Spring MVC\\Json Processing\\src\\main\\resources\\outputDataJson.json";
-
     public static void main(String[] args) {
+        Engine engine = new Engine();
 
-        FileReader reader = new FileReaderImpl();
-        FileWriter writer = new FileWriterImpl();
-        Gson gson = GsonProvider.getGson();
-
-
-        StationImportDto[] station;
-        String text;
-        try {
-            text = reader.readFile(IMPORT_DATA_PATH);
-        } catch (IOException e) {
-            e.printStackTrace();
-            // or do something;
-            return;
-        }
-
-        station = gson.fromJson(text, StationImportDto[].class);
-
-        Map<String, List<Double>> allStations = new LinkedHashMap<>();
-
-        Arrays.asList(station).forEach(stationImportDto -> {
-            allStations.putIfAbsent(stationImportDto.getStationName(), new ArrayList<>());
-            allStations.get(stationImportDto.getStationName()).add(stationImportDto.getPower());
-        });
-
-        List<StationExportDto> stationsExport = new ArrayList<>();
-        for (String stationName : allStations.keySet()) {
-
-            double average = getAverage(allStations.get(stationName));
-
-            StationExportDto dto = new StationExportDto(stationName, average);
-
-            stationsExport.add(dto);
-        }
-
-        String jsonText = gson.toJson(stationsExport);
-
-        try {
-            writer.write(EXPORT_DATA_PATH, jsonText);
-        } catch (IOException e) {
-            // Log print status... or something.
-            e.printStackTrace();
-        }
-    }
-
-    public static double getAverage(List<Double> allStations) {
-        return allStations
-                .stream()
-                .mapToDouble(x -> x)
-                .average()
-                .orElse(0.0);
+        engine.run();
     }
 }
