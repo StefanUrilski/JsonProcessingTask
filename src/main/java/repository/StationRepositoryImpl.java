4cb5a7148efe000a4c6f3fc8f0d2a5794a4cc0c8
StefanUrilski <urilskistefan@gmail.com>
2019-10-22 11:40:04 +0300

    StationRepository method addStationsFromJson() implemented.

--- src/main/java/repository/StationRepositoryImpl.java
+++ src/main/java/repository/StationRepositoryImpl.java
@@ -1,14 +1,33 @@
 package repository;
 
+import com.google.gson.Gson;
 import entity.Station;
 
-import java.util.List;
+import java.util.*;
 
 public class StationRepositoryImpl implements StationRepository {
 
+    private Map<String, List<Station>> data;
+
+    private Gson gson;
+
+    public StationRepositoryImpl(Gson gson) {
+        this.gson = gson;
+        this.data = new HashMap<>();
+    }
+
     @Override
     public String addStationsFromJson(String stationsJson) {
-        return null;
+        Station[] stations = gson.fromJson(stationsJson, Station[].class);
+
+        Arrays.stream(stations)
+                .forEach(station -> {
+                    Station currentStation = new Station(station.getName(), station.getAveragePower());
+                    data.putIfAbsent(station.getName(), new ArrayList<>());
+                    data.get(station.getName()).add(currentStation);
+                });
+
+        return String.format("%d stations, successfully added.", stations.length);
     }
 
     @Override
